5f420f5410e17692757a84f08728f514d9b223aa
SUBHAJIT SARKAR <ssubho.srkr@gmail.com>
2025-02-20 03:10:19 +0530

    Initial

---
+++ src/firebase.js
@@ -0,0 +1,43 @@
+import { initializeApp } from 'firebase/app';
+import { getToken, getMessaging, onMessage } from 'firebase/messaging';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbf-srviMaqq7hhTFJq7q775xHNmdm3Kc",
+  authDomain: "fir-push-notifications-a7941.firebaseapp.com",
+  projectId: "fir-push-notifications-a7941",
+  storageBucket: "fir-push-notifications-a7941.firebasestorage.app",
+  messagingSenderId: "363199561924",
+  appId: "1:363199561924:web:ef9fb4b3c659fca35bc991"
+};
+
+console.log('*** Environment ***', process.env.REACT_APP_ENV)
+console.log('*** Firebase Config ***', firebaseConfig)
+
+const firebaseApp = initializeApp(firebaseConfig);
+const messaging = getMessaging(firebaseApp);
+
+export const getOrRegisterServiceWorker = () => {
+  if ('serviceWorker' in navigator) {
+    return window.navigator.serviceWorker
+      .getRegistration('/firebase-push-notification-scope')
+      .then((serviceWorker) => {
+        if (serviceWorker) return serviceWorker;
+        return window.navigator.serviceWorker.register('/firebase-messaging-sw.js', {
+          scope: '/firebase-push-notification-scope',
+        });
+      });
+  }
+  throw new Error('The browser doesn`t support service worker.');
+};
+
+export const getFirebaseToken = () =>
+  getOrRegisterServiceWorker()
+    .then((serviceWorkerRegistration) =>
+      getToken(messaging, { vapidKey: 'BAJy-JyCTgNVXQShNtL-lJwo3TqgjjkWvHLtRiD_DM8Sel3K1J2g7g_-bNwTS5uvJt2pDrqcMt27FqpaTMniK48', serviceWorkerRegistration })
+        .then((currentToken) => {
+          console.log('Current token for client: ', currentToken);
+        })
+  );
+
+export const onForegroundMessage = () =>
+  new Promise((resolve) => onMessage(messaging, (payload) => resolve(payload)));
